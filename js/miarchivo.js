20c37743e2e3c6a6c92ba2e206ab13e785b8a0bc
marcelo bonda <marcelo.pf16@gmail.com>
2022-08-24 01:26:36 -0300

    desafio complementario 1

---
+++ js/miarchivo.js
@@ -0,0 +1,11 @@
+let balones = prompt("Hagamos una trivia! Cuantos balones de oro tiene messi?")
+
+while (balones !== "7"){
+alert("respuesta incorrecta!Probemos una vez mas");
+balones = prompt("cuantos balones de oro tiene messi");
+
+}
+
+if(balones==="7"){
+    alert("felicidades, es correcto!")
+}
